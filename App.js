d067311367348d6f0181d05e38df175f6e92cc81
Max Metral <max@pyralis.com>
2023-01-15 22:05:38 -0500

    Initial commit

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { observable } from "@legendapp/state"
+import { observer } from "@legendapp/state/react"
+
+const state = observable({ count: 1 });
+
+const Counter = observer(() => {
+  return (
+    <View>
+      <Text onPress={() => state.count.set(state.count.get() + 1)}>{state.count}</Text>
+    </View>
+  );
+})
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Counter />
       <StatusBar style="auto" />
     </View>
   );
---
+++ README.md
@@ -0,0 +1,16 @@
+# legend-esm-expo
+
+```
+npx create-expo-app legend-esm-expo
+cd legend-esm-expo
+npx expo install react-native-web@~0.18.9 react-dom@18.1.0 @expo/webpack-config@^0.17.2
+```
+
+[x] Verified app works in react-native-web
+
+```
+yarn add @legendapp/state
+```
+[x] Add observable to App.js and print a counter
+
+[ ] App no longer loads in web
